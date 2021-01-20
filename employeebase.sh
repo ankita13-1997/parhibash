d1e792b1540233dd9af9d681c084401b72aa39a3
ankita13-1997 <parhiankita@gmail.com>
2021-01-20 13:08:04 +0530

    employee salary is mentioned

--- employeebase.sh
+++ employeebase.sh
@@ -2,12 +2,12 @@
 echo "_________welcome employee to employee desk_____________"
 isPresent=1
 c=$(($RANDOM%2))
-salary=1000
+
 if [[ $c -eq $isPresent ]]
 then
-salary=$(($salary*20))
+salary=$((8*20))
 echo $salary
 else
-salary=$(($salary*8))
+salary=$((20*0))
 echo $salary
 fi
