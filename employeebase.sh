248b69b4c96ba2f844dc83230b7342e1c9a6fa82
ankita13-1997 <parhiankita@gmail.com>
2021-01-20 12:02:41 +0530

    employee attensence is checked

--- employeebase.sh
+++ employeebase.sh
@@ -1,1 +1,10 @@
+#! /bin/bash -x
 echo "_________welcome employee to employee desk_____________"
+isPresent=1
+c=$(($RANDOM%2))
+if [[ $c -eq $isPresent ]]
+then
+  echo"employee is present"
+else
+echo "employee is absent"
+fi
